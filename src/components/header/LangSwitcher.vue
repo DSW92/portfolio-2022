aaa78f6f76f43d0a89debe7e75d254d905f08ccf
Dominik Wojtowicz <dominik.s.wojtowicz@gmail.com>
2022-08-24 14:59:58 +0200

    language switcher created

--- src/components/header/LangSwitcher.vue
+++ src/components/header/LangSwitcher.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="select">
     <div class="selected-option" @click="toggleDropdown">
-      <span>{{ selectedLanguage }}</span>
+      <transition name="fade" mode="out-in">
+        <span :key="selectedLanguage">{{ selectedLanguage }}</span>
+      </transition>
       <span><img src="../../assets/chevron-down.png" alt="" /></span>
     </div>
     <div :class="{ dropdownVisible: optionsToggle }" class="options">
       <span
         v-if="selectedLanguage === 'en'"
-        value="pl"
         @click="
           changeSelectedLanguage('pl');
           toggleDropdown();
@@ -16,7 +17,6 @@
       >
       <span
         v-if="selectedLanguage === 'pl'"
-        value="en"
         @click="
           changeSelectedLanguage('en');
           toggleDropdown();
@@ -74,14 +74,18 @@ export default {
     flex-direction: row;
     border: 1px solid #222;
     padding: 2px 5px;
+    color: #222;
     span {
       text-transform: uppercase;
       display: flex;
       align-items: center;
+      font-weight: 500;
+    }
+    span:first-of-type {
+      width: 24px;
     }
     span:nth-of-type(2) {
       img {
-        transform: rotate(180deg);
         width: 10px;
         margin-left: 3px;
       }
@@ -94,16 +98,39 @@ export default {
     width: 100%;
     opacity: 0;
     pointer-events: none;
-    // transition-duration: 0.2s;
     span {
       cursor: pointer;
-      padding: 2px 5px;
+      padding: 5px 5px 2px 5px;
+      font-weight: 500;
+      color: #222;
     }
 
     &.dropdownVisible {
       opacity: 1;
       pointer-events: all;
+      transition-duration: 0.3s;
     }
   }
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-enter-active {
+  transition-duration: 0.3s;
+}
+
+// .fade-leave-from {
+//   display: none;
+// }
+// .fade-leave-to {
+//   display: none;
+// }
+
+.fade-leave-active {
+  display: none;
 }
 </style>
