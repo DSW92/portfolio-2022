acdf9ea2d993dd08d6b299187fe8b4a81d66ade7
Dominik Wojtowicz <dominik.s.wojtowicz@gmail.com>
2022-09-06 00:12:29 +0200

    my-offer done initially

--- src/components/MyOffer.vue
+++ src/components/MyOffer.vue
@@ -4,6 +4,7 @@
       <div class="row">
         <div class="col-12 col-md-7">
           <p>{{ $t("my-offer.paragraph-1") }}</p>
+          <p>{{ $t("my-offer.paragraph-1a") }}</p>
         </div>
         <div class="col-12 col-md-5">
           <div class="picture__container websites">
@@ -13,10 +14,23 @@
       </div>
       <div class="row">
         <div class="col-12 col-md-5">
-          <div class="picture__container"></div>
+          <div class="picture__container ecommerce">
+            <div></div>
+          </div>
         </div>
         <div class="col-12 col-md-7">
           <p>{{ $t("my-offer.paragraph-2") }}</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-md-7">
+          <p>{{ $t("my-offer.paragraph-3") }}</p>
+          <p>{{ $t("my-offer.paragraph-3a") }}</p>
+        </div>
+        <div class="col-12 col-md-5">
+          <div class="picture__container frontend">
+            <div></div>
+          </div>
         </div>
       </div>
     </div>
@@ -28,6 +42,15 @@
   padding: 5rem 0 0 0;
   .row {
     align-items: center;
+    margin-bottom: 3rem;
+    .col-12 {
+      p {
+        font-size: 1.5rem;
+        line-height: 1.7;
+        font-weight: 500;
+        margin: 0 0 1rem 0;
+      }
+    }
   }
 }
 
@@ -40,20 +63,45 @@
   &.websites {
     padding: 4.75rem 4.75rem 0 4.75rem;
     > div {
-      background-image: url("../assets/web-design.jpg");
+      background-image: url("../assets/website.png");
       background-position: center;
       background-repeat: no-repeat;
       background-size: cover;
       width: 100%;
       height: 100%;
-      min-height: 300px;
+      min-height: 250px;
       box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px,
         rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
     }
   }
-}
-.row {
-  margin-bottom: 5rem;
+  &.ecommerce {
+    padding: 4.75rem 4.75rem 0 4.75rem;
+    > div {
+      background-image: url("../assets/ecommerce.png");
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      width: 100%;
+      height: 100%;
+      min-height: 250px;
+      box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px,
+        rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+    }
+  }
+  &.frontend {
+    padding: 4.75rem 4.75rem 0 4.75rem;
+    > div {
+      background-image: url("../assets/programmer.png");
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      width: 100%;
+      height: 100%;
+      min-height: 250px;
+      box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px,
+        rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+    }
+  }
 }
 
 .theme-wrapper.dark-mode {
